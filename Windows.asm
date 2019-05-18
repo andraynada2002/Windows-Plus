9682ccc227f0812b61fb2ee233160ceece8c65f5
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-05-18 18:00:39 +1000

    Add files via upload

---
+++ Windows.asm
@@ -0,0 +1,310 @@
+mov asx ;CPU Kernel mode optimized
+mov asx ;CPU user mode optimized
+mov asx ;CPU instance optimized
+mov asx ;GPU Kernel mode optimized
+mov asx ;GPU user mode optimized
+mov asx ;GPU instance optimized
+mov asx ;RAM Kernel mode optimized
+mov asx ;RAM instance optimized
+mov asx ;RAM user mode optimized
+mov asx ;Windows Kernel mode optimized
+mov asx ;Windows user mode optimized
+mov asx ;Windows instance optimized
+mov asx ;Windows performance Kernel mode optimized
+mov asx ;Windows performance user mode optimized
+mov asx ;Windows performance instance optimized
+mov asx ;Infinite display resolution user mode optimized
+mov asx ;Infinite display resolution Kernel mode optimized
+mov asx ;Infinite display resolution instance optimized
+mov asx ;Infinite screen resolution user mode optimized
+mov asx ;Infinite screen resolution Kernel mode optimized
+mov asx ;Infinite screen resolution instance optimized
+mov asx ;Videomemory Kernel mode optimized
+mov asx ;Videomemory user mode optimized
+mov asx ;Videmomemory instance optimized
+mov asx ;Windows per process optimized
+mov asx ;Windows process X optimized
+mov asx ;Windows processes and resources Kernel mode optimized
+mov asx ;Windows processes and resources user mode optimized
+mov asx ;Windows processes and resources instance optimized
+mov asx ;Windows deadlock optimized
+mov asx ;Wi-Fi Kernel mode optimized
+mov asx ;Wi-Fi user mode optimized
+mov asx ;Wi-Fi instance optimized
+mov asx ;Wi-Fi connection Kernel mode optimized
+mov asx ;Wi-Fi connection user mode optimized
+mov asx ;Wi-Fi connection instance optimized
+mov asx ;Wi-Fi ping Kernel mode optimized
+mov asx ;Wi-Fi ping user mode optimized
+mov asx ;Wi-Fi ping instance optimized
+mov asx ;Wi-Fi speed Kernel mode optimized
+mov asx ;Wi-Fi speed user mode optimized
+mov asx ;Wi-Fi speed instance optimized
+mov asx ;Wi-Fi ultra connection Kernel mode optimized
+mov asx ;Wi-Fi ultra connection user mode
+mov asx ;Wi-Fi ultra connection instance
+mov asx ;Wi-Fi ultra speed user mode optimized
+mov asx ;Wi-Fi ultra speed instance optimized
+mov asx ;Wi-Fi ultra speed Kernel mode optimized
+mov asx ;Ethernet Kernel mode optimized
+mov asx ;Ethernet user mode optimized
+mov asx ;Ethernet instance optimized
+mov asx ;Ethernet connection Kernel mode optimized
+mov asx ;Ethernet connection instance optimized
+mov asx ;Ethernet connection user mode optimized
+mov asx ;Ethernet ping Kernel mode optimized
+mov asx ;Ethernet ping user mode optimized
+mov asx ;Ethernet ping instance optimized
+mov asx ;Ethernet ultra connection Kernel mode optimized
+mov asx ;Ethernet ultra connection user mode optimized
+mov asx ;Ethernet ultra connection instance optimized
+mov asx ;Ethernet ultra speed Kernel mode optimized
+mov asx ;Ethernet ultra speed user mode optimized
+mov asx ;Ethernet ultra speed instance optimized
+mov asx ;Windows microcore Kernel mode optimized
+mov asx ;Windows microcore user mode optimized
+mov asx ;Windows microcore instance optimized
+mov asx ;Windows monolithic core Kernel mode optimized
+mov asx ;Windows monolithic core user mode optimized
+mov asx ;Windows monolitchit core instance optimized
+mov asx ;Windows core Kernel mode optimized
+mov asx ;Windows core user mode optimized
+mov asx ;Windows core instance optimized
+mov asx ;Windows monolithic shell Kernel mode optimized
+mov asx ;Windows monolithit shell user mode optimized
+mov asx ;Windows monolithit shell instance optimized
+mov asx ;Windows microshell Kernel mode optimized
+mov asx ;Windows microshell user mode optimized
+mov asx ;Windows microshell instance optimized
+mov asx ;Windows shell Kernel mode optimized
+mov asx ;Windows shell user mode optimized
+mov asx ;Windows shell instance optimized
+mov asx ;Windows monolithic Kernel mode optimized
+mov asx ;Windows microKernel mode optimized
+mov asx ;Windows speed Kernel mode optimized
+mov asx ;Windows speed user mode optimized
+mov asx ;Windows speed instance optimized
+mov asx ;Windows realtime performance Kernel mode optimized
+mov asx ;Windows realtime performance user mode optimized
+mov asx ;Windows realtime performance instance optimized
+mov asx ;Windows realtime speed Kernel mode optimized
+mov asx ;Windows realtime speed user mode optimized
+mov asx ;Windows realtime speed instance optimized
+mov asx ;Windows realtime raytracing Kernel mode optimized
+mov asx ;Windows realtime raytracing user mode optimized
+mov asx ;Windows realtime raytracing instance optimized
+mov asx ;Windows raytracing Kernel mode optimized
+mov asx ;Windows raytracing user mode optimized
+mov asx ;Windows reaytracing instance optimized
+mov asx ;Windows toolbar Kernel mode optimized
+mov asx ;Windows toolbar user mode optimized
+mov asx ;Windows toolbar instance optimized
+mov asx ;Windows desktop user mode optimized
+mov asx ;Windows desktop Kernel mode optimized
+mov asx ;Windows desktop instance optimized
+mov asx ;Windows startmenu Kernel mode optimized
+mov asx ;Windows startmenu user mode optimized
+mov asx ;Windows startmenu instance optimized
+mov asx ;Windows infinite framerate user mode optimized
+mov asx ;Windows infinite framerate Kernel mode optimized
+mov asx ;Windows infinite framerate instance optimized
+mov asx ;Windows infinite fps user mode optimized
+mov asx ;Windows infinite fps Kernel mode optimized
+mov asx ;Windows infinite fps instance optimized
+mov asx ;Windows context menu user mode optimized
+mov asx ;Windows context menu Kernel mode optimized
+mov asx ;Windows context menu instance optimized
+mov asx ;Windows process waiting mode user mode optimized
+mov asx ;Windows process waiting mode Kernel mode optimized
+mov asx ;Windows process waiting mode instance optimized
+mov asx ;Windows process Kernel mode optimized
+mov asx ;Windows process instance optimized
+mov asx ;Windows process user mode optimized
+mov asx ;Windows process performance Kernel mode optimized
+mov asx ;Windows process performance user mode optimized
+mov asx ;Windows process performance instance optimized
+mov asx ;Windows resource Kernel mode optimized
+mov asx ;Windows resource user mode optimized
+mov asx ;Windows resource instance optimized
+mov asx ;Windows resource performance Kernel mode optimized
+mov asx ;Windows resource performance user mode optimized
+mov asx ;Windows resource performance instance optimized
+mov asx ;Internet Kernel mode optimized
+mov asx ;Internet user mode optimized
+mov asx ;Internet instance optimized
+mov asx ;Internet speed Kernel mode optimized
+mov asx ;Internet speed user mode optimized
+mov asx ;Internet speed instance optimized
+mov asx ;Internet ping Kernel mode optimized
+mov asx ;Internet ping user mode optimized
+mov asx ;Internet ping instance optimized
+mov asx ;Internet ping Kernel mode optimized
+mov asx ;Internet connection user mode optimized
+mov asx ;Internet connection Kernel mode optimized
+mov asx ;Internet connection instance optimized
+mov asx ;Internet ultra speed user mode optimized
+mov asx ;Internet ultra speed Kernel mode optimized
+mov asx ;Internet ultra speed instance optimized
+mov asx ;Internet ultra connection user mode optimized
+mov asx ;Internet ultra connection Kernel mode optimized
+mov asx ;Internet ultra connection instance optimized
+mov asx ;Audio Kernel mode optimized
+mov asx ;Audio user mode optimized
+mov asx ;Audio instance optimized
+mov asx ;Antivirus Kernel mode optimized
+mov asx ;Antivirus user mode optimized
+mov asx ;Antivirus instance optimized
+mov asx ;Antimalware Kernel mode optimized
+mov asx ;Antimalware user mode optimized
+mov asx ;Antimalware instance optimized
+mov asx ;Antitrojan Kernel mode optimized
+mov asx ;Antitrojan user mode optimized
+mov asx ;Antitrojan instance optimized
+mov asx ;Process speed Kernel mode optimized
+mov asx ;Process speed user mode optimized
+mov asx ;Process speed instance optimized
+mov asx ;Resource speed Kernel mode optimized
+mov asx ;Resource speed user mode optimized
+mov asx ;Resource speed instace optimized
+mov asx ;Interrupt Kernel mode optimized
+mov asx ;Interrupt user mode optimized
+mov asx ;Interrupt instance optimized
+mov asx ;Multiple exceptions Kernel mode optimized
+mov asx ;Multiple exceptions instance optimized
+mov asx ;Multiple exceptions user mode optimized
+mov asx ;Multiprogramming Kernel mode optimized
+mov asx ;Multiprogramming instance optimized
+mov asx ;Multiprogramming user mode optimized
+mov asx ;Secondary memory Kernel mode optimized
+mov asx ;Secondare memory instance optimized
+mov asx ;Secondary memory user mode optimized
+mov asx ;System bus Kernel mode optimized
+mov asx ;System bus instance optimized
+mov asx ;System bus user mode optimized
+mov asx ;Physical memory Kernel mode optimized
+mov asx ;Physical memory user mode optimized
+mov asx ;Physical memory instance optimized
+mov asx ;Virtual memory Kernel mode optimized
+mov asx ;Virtual memory user mode optimized
+mov asx ;Virtual memory instance optimized
+mov asx ;I/O modules Kernel mode optimized
+mov asx ;I/O modules user mode optimized
+mov asx ;I/O modules instance optimized
+mov asx ;Main memory Kernel mode optimized
+mov asx ;Main memory instance optimized
+mov asx ;Main memory user mode optimized
+mov asx ;Processor Kernel mode optimized
+mov asx ;Processor instance optimized
+mov asx ;Processor user mode optimized
+mov asx ;Computer Kernel mode optimized
+mov asx ;Computer instance optimized
+mov asx ;Computer user mode optimized
+mov asx ;Operating System Kernel mode optimized
+mov asx ;Operating System instance optimized
+mov asx ;operating System user mode optimized
+mov asx ;Physical Memory based operating system Kernel mode optimized
+mov asx ;Physical Memory based operating system instance optimized
+mov asx ;Physical Memory based operating system user mode optimized
+mov asx ;Virtual Memory based operating system Kernel mode optimized
+mov asx ;Virtual Memory based operating system instance optimized
+mov asx ;Virtual Memory based operating system user mode optimized
+mov asx ;Layers of abstraction Kernel mode optimized
+mov asx ;Layers of abstraction instance optimized
+mov asx ;Layers of abstraction user mode optimized
+mov asx ;Evolution OS Kernel mode optimized
+mov asx ;Evolution OS instance optimized
+mov asx ;Evolution OS user mode optimized
+mov asx ;Serial Processing Kernel mode optimized
+mov asx ;Serial Processing instance optimized
+mov asx ;Serial Processing user mode optimized
+mov asx ;Batch Processing Kernel mode optimized
+mov asx ;Batch Processing instance optimized
+mov asx ;Batch Processing user mode optimized
+mov asx ;Time Sharing system Kernel mode optimized
+mov asx ;Time sharing system instance optimized
+mov asx ;Time sharing system user mode optimized
+mov asx ;Serial Batch System Kernel mode optimized
+mov asx ;Serial Batch System user mode optimized
+mov asx ;Serail Batch System instance optimized
+mov asx ;Problems Kernel mode optimized
+mov asx ;Problems user mode optimized
+mov asx ;Problems instance optimized
+mov asx ;Read from file Kernel mode optimized
+mov asx ;Read from file user mode optimized
+mov asx ;Read from file instance optimized
+mov asx ;Run instructions Kernel mode optimized
+mov asx ;Run instructions user mode optimized
+mov asx ;Run instructions instance optimized
+mov asx ;Run 100 instructions Kernel mode optimized
+mov asx ;Run 100 instructions user mode optimized
+mov asx ;Run 100 instruction instance optimized
+mov asx ;Writing the result to a file Kernel mode optimized
+mov asx ;Writing the result to a file user mode optimized
+mov asx ;Writing the result to a file instance optimized
+mov asx ;CPU utilization kernel mode optimized
+mov asx ;CPU utilization user mode optimized
+mov asx ;CPU utilization instance optimized
+mov asx ;GPU utilization kernel mode optimized
+mov asx ;GPU utilization user mode optimized
+mov asx ;GPU utilization instance optimized
+mov asx ;RAM utilization kernel mode optimized
+mov asx ;RAM utilization user mode optimized
+mov asx ;RAM utilization instance optimized
+mov asx ;Uniprogramming instance optimized
+mov asx ;Uniprogramming user mode optimized
+mov asx ;Uniprogramming kernel mode optimized
+mov asx ;Time sharing systems kernel mode optimized
+mov asx ;Time sharing systems user mode optimized
+mov asx ;Time sharing systems instance optimized
+mov asx ;The operation mode of the CPU kernel mode optimized
+mov asx ;The operation mode of the CPU user mode optimized
+mov asx ;The operation mode of the CPU instance optimized
+mov asx ;Program in process of execution kernel mode optimized
+mov asx ;Program in process of execution user mode optimized
+mov asx ;Program in process of execution instance optimized
+mov asx ;Application startup kernel mode optimized
+mov asx ;Application startup user mode optimized
+mov asx ;Application startup instance optimized
+mov asx ;Processing Control Block kernel mode optimized
+mov asx ;Processing Control Block user mode optimized
+mov asx ;Processing Control Block instance optimized
+mov asx ;Simple model of process States kernel mode optimized
+mov asx ;Simple model of process States user mode optimized
+mov asx ;Simple model of process States instance optimized
+mov asx ;Queue kernel mode optimized
+mov asx ;Queue user mode optimized
+mov asx ;Queue instance optimized
+mov asx ;Destroyed process kernel mode optimized
+mov asx ;Destroyed process user mode optimized
+mov asx ;Destroyed process instance optimized
+mov asx ;five States kernel mode optimized
+mov asx ;five States user mode optimized
+mov asx ;five States instance optimized
+mov asx ;Secondary Queue user mode optimized
+mov asx ;Secondery Queue kernel mode optimized
+mov asx ;Secondery Queue instance optimized
+mov asx ;Multiple queue user mode optimized
+mov asx ;Multiple queue kernel mode optimized
+mov asx ;Multiple queue instance optimized
+mov asx ;Suspended processes optimized
+mov asx ;SIMD optimized
+mov asx ;MIMD optimized
+mov asx ;Mutex optimized
+mov asx ;Critical section optimized
+mov asx ;Starvation optimized
+mov asx ;Monitor optimized
+mov asx ;Scheduling tasks optimized
+mov asx ;Input and Output optimized
+mov asx ;Device input and output optimized
+mov asx ;Wi-Fi speed optimized
+mov asx ;Wi-Fi ping optimized
+mov asx ;Wi-Fi connection optimized
+mov asx ;Ethernet speed optimized
+mov asx ;Ethernet ping optimized
+mov asx ;Ethernet connection optimized
+mov asx ;Context menu optimized
+mov asx ;Windows optimized
+mov asx ;Processes optimized
+mov asx ;Resources optimized
+
+
